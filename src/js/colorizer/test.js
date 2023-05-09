b5048167ca536f0a68700238366cfc99577fd675
nylus-ark <nylus-ark@users.noreply.github.com>
2023-05-09 09:28:41 +0000

    deploy: ec0a012dbe1d7c472206fb13c465cd6ecc4d1b23

--- src/js/colorizer/test.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var h1 = document.querySelector("h1");
-  h1.addEventListener("click", function () {
-    h1.style.color = "red";
-  });
-});
